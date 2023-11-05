3f5bf60eebbf0d8f98d01d887c6df174c8c00f25
jianguang.wang <572707836@qq.com>
2023-11-05 23:50:53 +0800

    结合 javap 字节码学习泛型

--- core/src/main/java/com/wangjg/generic/SimpleGeneric.java
+++ core/src/main/java/com/wangjg/generic/SimpleGeneric.java
@@ -1,5 +1,6 @@
 package com.wangjg.generic;
 
+
 public class SimpleGeneric<T> {
 
     private T f1;
@@ -10,6 +11,18 @@ public class SimpleGeneric<T> {
 
     public void setF1(T f1) {
         this.f1 = f1;
+    }
+
+    public static <AnotherGeneric> AnotherGeneric echo(AnotherGeneric anotherGeneric){
+        return anotherGeneric;
+    }
+
+    public static <ObjectType> TypeReference<ObjectType> getTypeReference(TypeReference<ObjectType> typeReference) {
+        return typeReference;
+    }
+
+    public TypeReference<T> getTypeReference() {
+        return new TypeReference<T>(){};
     }
 
 }
--- core/src/main/java/com/wangjg/generic/TestGenericChild.java
+++ core/src/main/java/com/wangjg/generic/TestGenericChild.java
@@ -1,4 +1,14 @@
 package com.wangjg.generic;
 
 public class TestGenericChild extends SimpleGeneric<String> {
+
+    public static void main(String[] args) {
+        System.out.println(SimpleGeneric.echo("test"));
+        TypeReference<String> typeReference = SimpleGeneric.getTypeReference(new TypeReference<String>() {
+        });
+        System.out.println(typeReference);
+
+        TestGenericChild testGenericChild = new TestGenericChild();
+        System.out.println(testGenericChild.getTypeReference());
+    }
 }
